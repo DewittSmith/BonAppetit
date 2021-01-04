0fba18c65ef6d77ed1d4033bacce4b88b704ce9b
Disketaa <76956762+Disketaa@users.noreply.github.com>
2021-01-05 01:22:28 +0300

    Revert "Revert "last""
    This reverts commit 4bf79c9727bc08dc45c8f1cbea7c4aff9a768d5d.

--- src/main/java/net/bonappetit/registry/ModBlocks.java
+++ src/main/java/net/bonappetit/registry/ModBlocks.java
@@ -15,7 +15,13 @@ public class ModBlocks
     public static Map<String, SimpleBlock> Blocks;
     static {
         Blocks = new HashMap<>();
+        Blocks.put("HALITE_ORE", new SimpleBlock("halite_ore", Registry.BLOCK, FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 0).requiresTool().strength(3f, 3f).sounds(BlockSoundGroup.NETHER_GOLD_ORE)));
         Blocks.put("LIMESTONE", new SimpleBlock("limestone", Registry.BLOCK, FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 0).requiresTool().strength(1.25f, 5.0f).sounds(BlockSoundGroup.NETHERRACK)));
+        Blocks.put("LIMESTONE_POLISHED", new SimpleBlock("limestone_polished", Registry.BLOCK, FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 0).requiresTool().strength(1.5f, 6.0f).sounds(BlockSoundGroup.NETHERRACK)));
+        Blocks.put("QUARTZ_TILES", new SimpleBlock("quartz_tiles", Registry.BLOCK, FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 0).requiresTool().strength(0.8f, 0.8f).sounds(BlockSoundGroup.STONE)));
+        Blocks.put("SALT_BLOCK", new SimpleBlock("salt_block", Registry.BLOCK, FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 0).requiresTool().strength(1.5f, 1.5f).sounds(BlockSoundGroup.NETHERRACK)));
+        Blocks.put("BUTTER_BLOCK", new SimpleBlock("butter_block", Registry.BLOCK, FabricBlockSettings.of(Material.SOLID_ORGANIC).breakByTool(FabricToolTags.HOES, 0).strength(0.25f, 0.25f).sounds(BlockSoundGroup.HONEY).slipperiness(0.75f)));
+        Blocks.put("CHEESE_BLOCK", new SimpleBlock("cheese_block", Registry.BLOCK, FabricBlockSettings.of(Material.SOLID_ORGANIC).breakByTool(FabricToolTags.HOES, 0).strength(0.25f, 0.25f).sounds(BlockSoundGroup.HONEY)));
     }
     
     public static void registerBlocks()
