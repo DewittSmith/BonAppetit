4bf79c9727bc08dc45c8f1cbea7c4aff9a768d5d
Disketaa <76956762+Disketaa@users.noreply.github.com>
2021-01-05 01:20:11 +0300

    Revert "last"
    This reverts commit 80b5f6dda9b021138cb973b6f8ed480a4a22cc15.

--- src/main/java/net/bonappetit/registry/ModItems.java
+++ src/main/java/net/bonappetit/registry/ModItems.java
@@ -15,24 +15,22 @@ public class ModItems
     {
             // #Items
             new SimpleItem("salt", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP)),
-            new SimpleItem("cutting_board", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).maxCount(1)),
-            new SimpleItem("cooking_pan", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).maxCount(1)),
-            new SimpleItem("cooking_pot", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).maxCount(1)),
-            new SimpleItem("cooking_bakeware", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).maxCount(1)),
-            new SimpleItem("juicer", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).maxCount(1)),
+            new SimpleItem("cooking_pan", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP)),
+            new SimpleItem("cooking_pot", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP)),
+            new SimpleItem("cooking_bakeware", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP)),
+            new SimpleItem("juicer", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP)),
 
             // #Food
-            new SimpleItem("cream", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(1).saturationModifier(0.075f).build())),
-            new SimpleItem("butter", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(2).saturationModifier(0.2f).build())),
+            new SimpleItem("cream", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(1).saturationModifier(0.075f).build()),
+            new SimpleItem("butter", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(2).saturationModifier(0.2f).build()),
             new SimpleItem("cheese", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(2).saturationModifier(0.4f).build())),
-            new SimpleItem("cheese_croquettes", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(3).saturationModifier(0.4f).snack().build())),
-            new SimpleItem("cheese_soup", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(4).saturationModifier(0.6f).build())),
-            new SimpleItem("carrot_grated", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(4).saturationModifier(0.5f).build())),
-            new SimpleItem("juice_apple", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(6).saturationModifier(0.3f).snack().build())),
-            new SimpleItem("juice_pumpkin", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(4).saturationModifier(0.65f).snack().build())),
-            new SimpleItem("juice_carrot", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(4).saturationModifier(0.3f).snack().build())),
-            new SimpleItem("juice_melon", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(2).saturationModifier(0.25f).snack().build())),
-            new SimpleItem("juice_berry", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(2).saturationModifier(0.25f).snack().build())),
+            new SimpleItem("cheese_croquettes", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(3).saturationModifier(0.4f).snack().build()),
+            new SimpleItem("cheese_soup", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(4).saturationModifier(0.6f).build()),
+            new SimpleItem("juice_apple", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(1).saturationModifier(0.075f).snack().build()),
+            new SimpleItem("juice_berry", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(1).saturationModifier(0.075f).snack().build()),
+            new SimpleItem("juice_carrot", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(1).saturationModifier(0.075f).snack().build()),
+            new SimpleItem("juice_melon", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(1).saturationModifier(0.075f).snack().build()),
+            new SimpleItem("juice_pumpkin", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).food(new FoodComponent.Builder().hunger(1).saturationModifier(0.075f).snack().build()),
 
             // #Miscs
             new SimpleItem("music_disc_afternoon", Registry.ITEM, new Item.Settings().group(BonAppetit.ITEM_GROUP).rarity(Rarity.RARE)),
@@ -41,14 +39,7 @@ public class ModItems
     // Block Items
     public static final SimpleBlockItem[] BlockItems = new SimpleBlockItem[]
     {
-            // #Building Blocks
-            new SimpleBlockItem("halite_ore", Registry.ITEM, ModBlocks.Blocks.get("HALITE_ORE").Block, new Item.Settings().group(BonAppetit.ITEM_GROUP)),
-            new SimpleBlockItem("limestone", Registry.ITEM, ModBlocks.Blocks.get("LIMESTONE").Block, new Item.Settings().group(BonAppetit.ITEM_GROUP)),
-            new SimpleBlockItem("limestone_polished", Registry.ITEM, ModBlocks.Blocks.get("LIMESTONE_POLISHED").Block, new Item.Settings().group(BonAppetit.ITEM_GROUP)),
-            new SimpleBlockItem("quartz_tiles", Registry.ITEM, ModBlocks.Blocks.get("QUARTZ_TILES").Block, new Item.Settings().group(BonAppetit.ITEM_GROUP)),
-            new SimpleBlockItem("salt_block", Registry.ITEM, ModBlocks.Blocks.get("SALT_BLOCK").Block, new Item.Settings().group(BonAppetit.ITEM_GROUP)),
-            new SimpleBlockItem("butter_block", Registry.ITEM, ModBlocks.Blocks.get("BUTTER_BLOCK").Block, new Item.Settings().group(BonAppetit.ITEM_GROUP)),
-            new SimpleBlockItem("cheese_block", Registry.ITEM, ModBlocks.Blocks.get("CHEESE_BLOCK").Block, new Item.Settings().group(BonAppetit.ITEM_GROUP)),
+        new SimpleBlockItem("limestone", Registry.ITEM, ModBlocks.Blocks.get("LIMESTONE").Block, new Item.Settings().group(BonAppetit.ITEM_GROUP)),
     };
 
     public static void registerItems()
@@ -56,5 +47,5 @@ public class ModItems
         for (SimpleBlockItem blockItem : BlockItems) Registry.register(blockItem.Registry, blockItem.Identifier, blockItem.BlockItem);
         for (SimpleItem item : Items) Registry.register(item.Registry, item.Identifier, item.Item);
 
-    }
+    };
 }
