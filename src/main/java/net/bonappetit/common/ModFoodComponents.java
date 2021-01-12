6c6b88f48bc525917a0bbbdc8e7d88a9e4c98c33
Disketaa <76956762+Disketaa@users.noreply.github.com>
2021-01-13 01:52:58 +0300

    Beta 8.3.1

--- src/main/java/net/bonappetit/common/ModFoodComponents.java
+++ src/main/java/net/bonappetit/common/ModFoodComponents.java
@@ -3,9 +3,9 @@ package net.bonappetit.common;
 import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
-    public static final FoodComponent LEEK = new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).build();
-    public static final FoodComponent TOMATO = new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).build();
+    public static final FoodComponent SCALLION = new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).build();
     public static final FoodComponent ONION = new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).build();
+    public static final FoodComponent TOMATO = new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).build();
     public static final FoodComponent GARLIC = new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).build();
     public static final FoodComponent CHEESE = new FoodComponent.Builder().hunger(2).saturationModifier(0.4f).build();
     public static final FoodComponent CHEESE_CROQUETTES = new FoodComponent.Builder().hunger(2).saturationModifier(0.3f).snack().build();
@@ -18,6 +18,7 @@ public class ModFoodComponents {
     public static final FoodComponent JUICE_APPLE = new FoodComponent.Builder().hunger(4).saturationModifier(0.2f).alwaysEdible().build();
     public static final FoodComponent JUICE_PUMPKIN = new FoodComponent.Builder().hunger(5).saturationModifier(0.2f).alwaysEdible().build();
     public static final FoodComponent JUICE_CARROT = new FoodComponent.Builder().hunger(3).saturationModifier(0.2f).alwaysEdible().build();
+    public static final FoodComponent JUICE_TOMATO = new FoodComponent.Builder().hunger(2).saturationModifier(0.1f).alwaysEdible().build();
     public static final FoodComponent JUICE_MELON = new FoodComponent.Builder().hunger(2).saturationModifier(0.1f).alwaysEdible().build();
     public static final FoodComponent JUICE_BERRIES = new FoodComponent.Builder().hunger(2).saturationModifier(0.1f).alwaysEdible().build();
 }
