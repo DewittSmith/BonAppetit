868ee98e98266bb6d37980fa5421e3fca1030280
DewittSmith <dewittsmith1337@gmail.com>
2021-01-13 20:41:46 +0700

    TransperentCrops

--- src/main/java/net/bonappetit/common/ModCropBlock.java
+++ src/main/java/net/bonappetit/common/ModCropBlock.java
@@ -1,9 +1,12 @@
 package net.bonappetit.common;
 
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.block.CropBlock;
+import net.minecraft.client.render.RenderLayer;
 
 public class ModCropBlock extends CropBlock {
     public ModCropBlock(Settings settings) {
         super(settings.nonOpaque());
+        BlockRenderLayerMap.INSTANCE.putBlock(this, RenderLayer.getCutout());
     }
 }
